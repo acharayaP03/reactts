dda66b08ad2c41dbd3a072c407361ec57706ee75
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-14 20:20:03 +1100

    better organise actions and actions types

--- src/ReduxStore/reducers/repositoriesReducers.ts
+++ src/ReduxStore/reducers/repositoriesReducers.ts
@@ -2,32 +2,13 @@
  * Respositories Reduders
  */
 
+import { ActionType} from "../Action-Types";
+import { Action } from "../Actions";
+
 interface RepositoriesState {
     loading: boolean;
     error: string | null;
     data: string[]
-}
-
-interface SearchRepositoriesAction {
-    type: ActionType.SEARCH_REPOSITORIES
-}
-
-interface SearchRepositoriesSuccessAction {
-    type: ActionType.SEARCH_REPOSITORIES_SUCCESS
-    payload: string[]
-}
-
-interface SearchRepositoriesErrorAction {
-    type: ActionType.SEARCH_REPOSITORIES_ERROR;
-    payload: string
-}
-
-type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction
-
-enum ActionType {
-    SEARCH_REPOSITORIES = 'search_repositories',
-    SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
-    SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
 }
 
 /**
