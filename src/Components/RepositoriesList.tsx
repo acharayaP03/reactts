5bb6386577d431518f23269b064521d0bad336f8
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-14 22:00:50 +1100

    Cusotm hooks to combine actions creators

--- src/Components/RepositoriesList.tsx
+++ src/Components/RepositoriesList.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import { useActions } from "../hooks/useActions";
+
 
 const RepositoriesList = (): JSX.Element =>{
+    const [term, setTerm] = useState('');
+    const {searchRepositories } = useActions();
 
-    return <h1>Repositories list</h1>
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        searchRepositories(term);
+    }
+    return (
+        <div>
+            <h1>Repositories list</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={term} onChange={(e) => setTerm(e.target.value)}/>
+                <button type="submit">Search Repo</button>
+            </form>
+        </div>
+    )
 }
 
 
