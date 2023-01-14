09647a8dd3dcd469f77eab1b187274884cb5227d
TrishtenAcharya <acharyaP03@gmail.com>
2023-01-14 22:30:54 +1100

    list out all the results

--- src/Components/RepositoriesList.tsx
+++ src/Components/RepositoriesList.tsx
@@ -1,14 +1,17 @@
 import React, {useState} from "react";
 import { useActions } from "../hooks/useActions";
-
+import {useSelector} from "react-redux";
+import { useTypedSelector } from '../hooks/useTypedSelector'
 
 const RepositoriesList = (): JSX.Element =>{
     const [term, setTerm] = useState('');
     const {searchRepositories } = useActions();
+    const {loading, error, data } = useTypedSelector((state) => state.repositories)
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
         e.preventDefault();
         searchRepositories(term);
+        console.log(data)
     }
     return (
         <div>
@@ -16,6 +19,10 @@ const RepositoriesList = (): JSX.Element =>{
             <form onSubmit={handleSubmit}>
                 <input type="text" value={term} onChange={(e) => setTerm(e.target.value)}/>
                 <button type="submit">Search Repo</button>
+                { error && <h3>{ error }</h3>}
+                {loading && <h3>Loading....</h3>}
+                {!error && !loading &&
+                    data.map(name => <div key={name} className="list-items">{ name }</div>) }
             </form>
         </div>
     )
